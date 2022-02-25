4184fc1b53aa819fb622ff2bf89a4fec875e9abe
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-02-25 18:14:18 +0900

    componentAttributeBag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -7,4 +7,5 @@
 
     <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル2"></x-tests.card>
+    <x-tests.card title="タイトル3" class="bg-red-500"></x-tests.card>
 </x-tests.app>
