2095fb2556d2492b1c0a37052ad964a51c7b8400
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-03-02 02:14:06 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -73,7 +73,10 @@ class OwnersController extends Controller
 
         return redirect()
             ->route('admin.owners.index')
-            ->with('message', '登録が完了しました。');
+            ->with([
+                'message' => '編集が完了しました。',
+                'status' => 'info',
+            ]);
     }
 
     /**
@@ -123,7 +126,10 @@ class OwnersController extends Controller
 
         return redirect()
             ->route('admin.owners.index')
-            ->with('message', '編集が完了しました。');
+            ->with([
+                'message' => '編集が完了しました。',
+                'status' => 'info',
+            ]);
     }
 
     /**
@@ -134,6 +140,25 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+
+        return redirect()
+            ->route('admin.owners.index')
+            ->with([
+                'message' => '削除が完了しました。',
+                'status' => 'alert',
+            ]);
+    }
+
+    public function expiredOwnerIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
     }
 }
