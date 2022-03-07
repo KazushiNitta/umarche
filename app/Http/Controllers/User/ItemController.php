a6be7a5f18c3ab5ff4f1b95a35e6d00cb5c811af
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-03-08 04:20:44 +0900

    showUpdate

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -28,5 +33,12 @@ class ItemController extends Controller
         ->get();
 
         return view('user.index', compact('stocks', 'products'));
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
