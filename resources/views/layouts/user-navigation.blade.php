e34ae02946163af76d00a5697e5c84cabee51174
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-03-09 15:06:52 +0900

    cartIndex

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -18,6 +18,11 @@
                         {{ __('ホーム') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('user.cart.index')" :active="request()->routeIs('user.cart.index')">
+                        {{ __('カート') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -67,6 +72,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('user.items.index')" :active="request()->routeIs('user.imtems.index')">
                 {{ __('ホーム') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('user.cart.index')" :active="request()->routeIs('user.cart.index')">
+                {{ __('カート') }}
             </x-responsive-nav-link>
         </div>
 
