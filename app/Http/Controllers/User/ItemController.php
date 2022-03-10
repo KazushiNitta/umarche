7187ca7d6a7a4ebab5c402d22a94b3e74dfc93cc
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-03-10 16:46:10 +0900

    searchFormCategory

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use App\Models\PrimaryCategory;
 
 class ItemController extends Controller
 {
@@ -28,9 +29,13 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        $products = Product::AvailableItems()->sortOrder($request->sort)->paginate($request->pagination ?? '20');
+        $categories = PrimaryCategory::with('secondary')->get();
+        $products = Product::AvailableItems()
+            ->selectCategory($request->category ?? '0')
+            ->sortOrder($request->sort)
+            ->paginate($request->pagination ?? '20');
 
-        return view('user.index', compact('products'));
+        return view('user.index', compact('products', 'categories'));
     }
 
     public function show($id)
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,34 +1,49 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                ホーム
-            </h2>
-            <div>
-                <form method="get" action="{{ route('user.items.index') }}">
-                    <div class="flex">
-                        <div>
-                            <span class="text-sm">表示順</span><br>
-                            <select name="sort" id="sort" class="mr-4">
-                                <option value="0" @if (\Request::get('sort') === '0') selected @endif>おすすめ順</option>
-                                <option value="1" @if (\Request::get('sort') === '1') selected @endif>料金が高い順</option>
-                                <option value="2" @if (\Request::get('sort') === '2') selected @endif>料金が安い順</option>
-                                <option value="3" @if (\Request::get('sort') === '3') selected @endif>新しい順</option>
-                                <option value="4" @if (\Request::get('sort') === '4') selected @endif>古い順</option>
-                            </select>
-                        </div>
-                        <div>
-                            <span class="text-sm">表示件数</span><br>
-                            <select name="pagination" id="pagination" class="mr-4">
-                                <option value="20" @if (\Request::get('pagination') === '20') selected @endif>20件</option>
-                                <option value="50" @if (\Request::get('pagination') === '50') selected @endif>50件</option>
-                                <option value="100" @if (\Request::get('pagination') === '100') selected @endif>100件</option>
-                            </select>
-                        </div>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            ホーム
+        </h2>
+        <form method="get" action="{{ route('user.items.index') }}">
+            <div class="lg:flex lg:justify-around">
+                <div class="lg:flex items-center">
+                    <select name="category" id="" class="mb-2 lg:mb-0 lg:mr-2">
+                        <option value="0" @if (\Request::get('category') === '0') selected @endif>全て</option>
+                        @foreach ($categories as $category)
+                            <optgroup label="{{ $category->name }}">
+                            @foreach ($category->secondary as $secondary)
+                                <option value="{{ $secondary->id }}" @if (\Request::get('category') == $secondary->id) selected @endif>
+                                    {{ $secondary->name }}
+                                </option>
+                            @endforeach
+                        @endforeach
+                    </select>
+                    <div class="flex space-x-2 items-center">
+                        <div><input placeholder="キーワードを入力" type="" name="keyword" class="border border-gray-500 py-2"></div>
+                        <div><button class="ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">検索</button></div>
                     </div>
-                </form>
+                </div>
+                <div class="flex space-x-2 items-center">
+                    <div>
+                        <span class="text-sm">表示順</span><br>
+                        <select name="sort" id="sort" class="mr-4">
+                            <option value="0" @if (\Request::get('sort') === '0') selected @endif>おすすめ順</option>
+                            <option value="1" @if (\Request::get('sort') === '1') selected @endif>料金が高い順</option>
+                            <option value="2" @if (\Request::get('sort') === '2') selected @endif>料金が安い順</option>
+                            <option value="3" @if (\Request::get('sort') === '3') selected @endif>新しい順</option>
+                            <option value="4" @if (\Request::get('sort') === '4') selected @endif>古い順</option>
+                        </select>
+                    </div>
+                    <div>
+                        <span class="text-sm">表示件数</span><br>
+                        <select name="pagination" id="pagination" class="mr-4">
+                            <option value="20" @if (\Request::get('pagination') === '20') selected @endif>20件</option>
+                            <option value="50" @if (\Request::get('pagination') === '50') selected @endif>50件</option>
+                            <option value="100" @if (\Request::get('pagination') === '100') selected @endif>100件</option>
+                        </select>
+                    </div>
+                </div>
             </div>
-        </div>
+        </form>
     </x-slot>
 
     <div class="py-12">
