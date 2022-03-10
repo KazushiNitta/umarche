fd7ba7f70c44bb8a6ac526fb45cb4176f33b4441
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-03-10 14:07:26 +0900

    localScope

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -17,23 +17,9 @@ class ItemController extends Controller
 
     public function index()
     {
-        $stocks = DB::table('t_stocks')
-        ->select('product_id', DB::raw('sum(quantity) as quantity'))
-        ->groupBy('product_id')
-        ->having('quantity', '>', 1);
+        $products = Product::AvailableItems()->get();
 
-        $products = DB::table('products')
-        ->joinSub($stocks, 'stock', function($join){
-            $join->on('products.id', '=', 'stock.product_id');
-        })
-        ->join('shops', 'products.shop_id', '=', 'shops.id')
-        ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
-        ->where('shops.is_selling', true)
-        ->where('products.is_selling', true)
-        ->select('products.id as id', 'products.name as name', 'products.price','products.sort_order as sort_order', 'products.information', 'secondary_categories.name as category')
-        ->get();
-
-        return view('user.index', compact('stocks', 'products'));
+        return view('user.index', compact('products'));
     }
 
     public function show($id)
